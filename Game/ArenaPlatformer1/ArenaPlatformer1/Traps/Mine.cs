c1a456006274dd192f2c8556e68f86f89e3b32e9
Gryff David <darthmarshie@gmail.com>
2020-09-07 10:44:32 +0200

    Added a mine trap class

---
+++ Game/ArenaPlatformer1/ArenaPlatformer1/Traps/Mine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace ArenaPlatformer1
+{
+    class Mine : Trap
+    {
+        public Mine()
+        {
+            TrapType = TrapType.Mine;
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            DestinationRectangle = new Rectangle((int)Position.X, (int)Position.Y, 32, 16);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, DestinationRectangle, Color.Red);
+        }
+    }
+}
